506b647519275bf291b16f72ba4bd80cec5cd909
tolulopeoduro <49287935+tolulopeoduro@users.noreply.github.com>
2022-12-26 16:43:42 +0000

    Added signup endpoint

--- api/src/controllers/onboarding/signup.js
+++ api/src/controllers/onboarding/signup.js
@@ -1,20 +1,29 @@
-import { jwt } from "jsonwebtoken";
+import { jwt, sign } from "jsonwebtoken";
 import { hash, genSalt, compare } from "bcrypt";
 import { ObjectID } from "bson";
 
 const signup = async (req, res, next) => {
     const client = req.app.locals.db;
 
-    const p = await client.db("twitter-clone").collection("users").countDocuments({ email: req.body.email });
-    if (p > 0) {
-        return res.status(401).json({ message: "email already used" });
+    console.log(req.body);
+
+    if (req.body.email) {
+        const email_exists = (await client.db("twitter-clone").collection("users").countDocuments({ email: req.body.email })) > 0;
+        if (email_exists) return res.status(401).json({ message: "email already used" });
     }
+
+    if (req.body.phone) {
+        const phone_exists = (await client.db("twitter-clone").collection("users").countDocuments({ email: req.body.email })) > 0;
+        if (phone_exists) {
+            return res.status(401).json({ message: "phone number already used" });
+        }
+    }
+
     hash(req.body.password, 10, async (err, hash) => {
         if (err) return res.status(500).json({ message: "Something went wrong, please try again" });
         req.body.password = hash;
         const newUser = await client.db("twitter-clone").collection("users").insertOne(req.body);
-        const token = await jwt.sign({ userId: newUser.insertedId }, "my-string", { expiresIn: "24h" });
-        console.log(user);
+        const token = await sign({ userId: newUser.insertedId }, "my-string", { expiresIn: "24h" });
         res.status(201).json({
             userId: newUser.insertedId,
             token: token,
