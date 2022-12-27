427af7071aea376cab0cea0f783784e27d936f3b
tolulopeoduro <49287935+tolulopeoduro@users.noreply.github.com>
2022-12-27 10:35:47 +0000

    added login endpoint

---
+++ api/src/controllers/onboarding/login.js
@@ -0,0 +1,33 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+
+const signup = async (req, res, next) => {
+    const client = req.app.locals.db;
+    const data = req.body.email ? { email: req.body.email } : req.body.phone ? { phone: req.body.phone } : null;
+    client
+        .db("twitter-clone")
+        .collection("users")
+        .findOne(data)
+        .then((user) => {
+            if (!user) {
+                return res.status(404).json({
+                    message: "user does not exist",
+                });
+            }
+            compare(req.body.password, user.password).then((valid) => {
+                if (!valid) {
+                    return res.status(401).json({
+                        message: "incorrect login details",
+                    });
+                }
+                const token = sign({ userId: user._id }, "my-string", { expiresIn: "24h" });
+                res.status(200).json({
+                    userId: user._id,
+                    token: token,
+                });
+            });
+            console.log(user);
+        });
+};
+
+export default signup;
--- api/src/routes/onboarding.js
+++ api/src/routes/onboarding.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import login from "../controllers/onboarding/login";
 import signup from "../controllers/onboarding/signup";
 
 const onboarding = new Router();
 
-onboarding.post("/login", (req, res, next) => {});
+onboarding.post("/login", login);
 onboarding.post("/signup", signup);
 
 export default onboarding;
